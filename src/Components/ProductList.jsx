6d4fcb071bb8230e5f0295a34af4670d93906a42
Ahmedns09 <anskhzyan@gmail.com>
2025-08-15 04:49:36 -0400

    added callback function to the button to add items to the cart and disable the button after adding form the ProductList component

--- src/Components/ProductList.jsx
+++ src/Components/ProductList.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
+
+import { useSelector, useDispatch } from 'react-redux';
+import { addItemToCart } from './CartSlice';
+
 import './ProductList.css';
 
+
 const ProductList = () => {
+
+    const dispatch = useDispatch();
+    const cartItems = useSelector(state => state.cart.cartItems); // Get cart items globally
 
     const products = [
         { id: 1, name: 'Product A', price: 60 },
         { id: 2, name: 'Product B', price: 75 },
         { id: 3, name: 'Product C', price: 30 },
     ];
+
+    const handleAddToCart = product => {
+        dispatch(addItemToCart(product));// Add product to cart
+    };
 
     return (
         <div className="product-list">
@@ -17,7 +29,14 @@ const ProductList = () => {
                 {products.map((product) => (
                     <li key={product.id} className="product-list-item">
                         <span> {product.name} - ${product.price} </span>
-                        <button> Add to Cart </button>
+
+                        <button
+                            className={`add-to-cart-btn ${cartItems.some(item => item.id === product.id) ? 'disabled' : ''}`}
+                            onClick={() => handleAddToCart(product)}
+                            disabled={cartItems.some(item => item.id === product.id)} // Disable if already in cart
+                        >
+                            {cartItems.some(item => item.id === product.id) ? "Added" : "Add to Cart"}
+                        </button>
                     </li>
                 ))}
 
